4e25c1cc3941a0c7300344870c2fcf4725d633c8
zabronax <zabronax@gmail.com>
2024-01-16 19:46:44 +0000

    configured vite to build in library mode

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,30 @@
+/* eslint-env node */
+import { resolve } from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.jsx"),
+      name: "MobiusContinuum",
+      fileName: "mobius-continuum",
+    },
+    rollupOptions: {
+      external: [
+        "react",
+        "react/jsx-runtime",
+        "react-dom",
+      ],
+      output: {
+        globals: {
+          'react': 'React',
+          'react/jsx-runtime': 'react/jsx-runtime',
+          'react-dom': 'ReactDOM',
+        }
+      },
+    }
+  }
 });
